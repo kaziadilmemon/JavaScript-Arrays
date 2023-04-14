e5a5a40d626a9a76af876518177a1d4f499686d1
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-15 01:08:45 +0500

    Create Arrays in JavaScript.js

---
+++ Arrays in JavaScript.js
@@ -0,0 +1,16 @@
+/* In JavaScript Arrays are variables which can hold more than one value.*/
+let marks=[19,82,63,84,false,"Not Present"]
+console.log(marks[0])
+console.log(marks[1])
+console.log(marks[2])
+console.log(marks[3])
+console.log(marks[4])
+console.log(marks[5])
+console.log(marks[6])
+console.log("The length of marks is",marks.length)
+marks=89 // Adding a new value to the array.
+marks=96 // Changing the value of an array.
+
+console.log(marks)
+console.log(typeof marks)
+/*
