a6651c740062ae7ab1d73de317d0e3ada7c993ca
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-15 18:42:32 +0500

    Update Practice Question no : 1.js

--- Practice Question no : 1.js
+++ Practice Question no : 1.js
@@ -6,3 +6,17 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 83]
  arr.push(a)
  console.log(arr)
 
+
+/* Keep Adding numbers to the array in above question untill 0 is added to the array: */
+
+let arr=[1,2,3,4,5,6,7,83]
+let a;
+do{
+    a = prompt("Enter a number")
+ a = Number.parseInt(a)
+ arr.push(a)
+}
+  while(a!=0)
+console.log(arr)
+
+
