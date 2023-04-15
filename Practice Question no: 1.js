dd75f4dd40a1b37d05392d25af49fb626dbec950
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-15 18:23:40 +0500

    Create Practice Question no: 1.js

---
+++ Practice Question no: 1.js
@@ -0,0 +1,7 @@
+/* Question : Create an array of numbers and take input from the user to add numbers to this array: */
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 83]
+ let a = prompt("Enter a number")
+ a = Number.parseInt(a)
+ arr.push(a)
+ console.log(arr)
