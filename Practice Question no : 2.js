96230b08c4f3729a353ee794032bfee5d4934eac
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-15 18:59:05 +0500

    Update Practice Question no : 2.js

--- Practice Question no : 2.js
+++ Practice Question no : 2.js
@@ -1,4 +1,4 @@
-// Question no : 3 filter for numbers divisible by 10 from a given array.
+// Question no : 2 filter for numbers divisible by 10 from a given array.
 
 let arr=[1,2,30,4,50,6,7,83,670]
 let n = arr.filter((x)=>{
